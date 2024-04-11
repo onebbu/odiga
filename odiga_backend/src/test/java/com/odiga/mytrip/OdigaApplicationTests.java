364ca95c70e1cc4290843061eb4d76740f81f30e
onebbu <vmfdo23@gmail.com>
2024-04-11 12:33:36 +0900

    [feature/setting] UPDATE: test 패키지 및 자바 파일명 수정

--- odiga_backend/src/test/java/com/odiga/mytrip/OdigaApplicationTests.java
+++ odiga_backend/src/test/java/com/odiga/mytrip/OdigaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ogada.mytrip;
+package com.odiga.mytrip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Ogada2ApplicationTests {
+class OdigaApplicationTests {
 
     @Test
     void contextLoads() {
