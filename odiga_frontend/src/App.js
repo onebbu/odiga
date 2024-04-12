3b907f9f956fa709bf20fe29bd3719d72a5faaad
gyuri <rbfl4759@gmail.com>
2024-04-12 11:42:41 +0900

    ADD choosePage 페이지 파일 첨부 ADD styled-components 패키지 다운로드 UPDATE 'App.js' choosePage path 추가

--- odiga_frontend/src/App.js
+++ odiga_frontend/src/App.js
@@ -5,8 +5,8 @@ import Login from "./pages/login/Login";
 import BoardList from "./pages/boardList/BoardList";
 import SignUp from "./pages/sign-up/SignUp";
 import TravelDetailPage from './pages/traveldetailpage/TravelDetailPage';
-import Test from './pages/traveldetailpage/Test';
 import ResultList from "./pages/result-list/ResultList";
+import ChoosePreference from './pages/choosePreference/ChoosePage';
 
 
 function App() {
@@ -19,8 +19,8 @@ function App() {
                     <Route exact path="/sign-up" element={<SignUp />}></Route>
                     <Route exact path="/board-list" element={<BoardList />}></Route>
                     <Route exact path="/detail" element={<TravelDetailPage />}></Route>
-                    <Route exact path="/test" element={<Test />}></Route>
                     <Route exact path="/result-list" element={<ResultList />}></Route>
+                    <Route exact path="/preference" element={<ChoosePreference />}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
