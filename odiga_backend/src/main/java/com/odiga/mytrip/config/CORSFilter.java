27637af630a7584983b0463fac8e60ab2262644f
onebbu <vmfdo23@gmail.com>
2024-04-17 01:04:17 +0900

    CORS 설정 변경

--- odiga_backend/src/main/java/com/odiga/mytrip/config/CORSFilter.java
+++ odiga_backend/src/main/java/com/odiga/mytrip/config/CORSFilter.java
@@ -8,20 +8,20 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 public class CORSFilter {
 
-    //CORS 필터
-    // @Bean
-    // public FilterRegistrationBean corsFilter() {
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     CorsConfiguration config = new CorsConfiguration();
-    //     config.setAllowCredentials(true);
-    //     config.addAllowedOrigin("*");
-    //     config.addAllowedHeader("*");
-    //     config.addAllowedMethod("*");
-    //     source.registerCorsConfiguration("/**", config);
-    //
-    //     FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-    //     registrationBean.setFilter(new CorsFilter());
-    //     registrationBean.setOrder(0);
-    //     return registrationBean;
-    // }
+    // CORS 필터
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.setOrder(0);
+        return registrationBean;
+    }
 }
