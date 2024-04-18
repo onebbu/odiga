a93fff3f7fbfcef9473992ef36bb293acdac9aa9
onebbu <vmfdo23@gmail.com>
2024-04-18 15:13:00 +0900

    [feature/login] 오타 수정

--- odiga_frontend/src/pages/login/Login.jsx
+++ odiga_frontend/src/pages/login/Login.jsx
@@ -95,7 +95,7 @@ export default function Login() {
                             <LockOutlinedIcon/>
                         </Avatar>
                         <Typography component="h1" variant="h5">
-                            회원가입
+                            로그인
                         </Typography>
                         <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
                             <Grid container spacing={3}>
