0766a614edeee5fef4ea6a246e522e46c30c4d1b
sejong6033 <tpwhd6033@naver.com>
2024-04-23 17:40:25 +0900

    ADD : 내여행목록 가져와서 보여주는 기능 추가(~ing)

--- odiga_backend/src/main/java/com/odiga/mytrip/course/controller/CourseDisplayController.java
+++ odiga_backend/src/main/java/com/odiga/mytrip/course/controller/CourseDisplayController.java
@@ -1,6 +1,7 @@
 package com.odiga.mytrip.course.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,9 @@ import com.odiga.mytrip.course.vo.CourseVO;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -18,11 +22,18 @@ public class CourseDisplayController {
     @Autowired
     private CourseService courseService;
 
-    @GetMapping("/CourseDisplay")
-    public List<CourseVO> getMethodName(@RequestParam String nickname) {
-        System.out.println(nickname);
+    @PostMapping("/MyCourseDisplay")
+    public List<CourseVO> getMethodName(@RequestBody Map<String, String> requestData) {
+        String nickname = requestData.get("nickname");
+        System.out.println("닉네임 : " + nickname +"가 접근시도");
         List<CourseVO> courseInfo = courseService.courselist(nickname);
-        System.out.println(courseInfo);
+        // System.out.println(courseInfo);
         return courseInfo;
     }
+    @PostMapping("/courseimport")
+    public String postMethodName(@RequestBody String entity) {
+        System.out.println(entity);
+        return entity;
+    }
+    
 }
