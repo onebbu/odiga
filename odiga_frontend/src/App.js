0f0166bf4ba4ba6799094b642678cf7bf3a8c160
Sangeun Park <101400455+onebbu@users.noreply.github.com>
2024-04-30 08:54:06 +0900

    Feature/resultlist UPDATE: 로딩 페이지 추가 및 target 폴더 삭제 (#47)
    * ADD: 로그인 페이지 추가
    
    * ADD: 회원가입 페이지 생성
    
    * ADD: 결과 조회 페이지 생생
    
    * [feature/resultlist] UPDATE: 여행지 결과 페이지 UI 수정
    
    * [feature/login] 오타 수정
    
    * [feature/login] UDATE: 오타 수정
    
    * [feature/resultlist] ADD: 결과 페이지용 데이터 가공 및 정리
    
    * [feature/resultlist] UPDATE: 지도 및 여행지 조회 1차 완료
    
    * [feature/resultlist] UPDATE: 백단에서 데이터 가공 부분 수정
    
    * UPDATE: 로딩 페이지 추가 및 target 폴더 삭제
    
    * conflict resolve

--- odiga_frontend/src/App.js
+++ odiga_frontend/src/App.js
@@ -21,14 +21,14 @@ function App() {
                     <Route exact path="/login" element={<Login />}></Route>
                     <Route exact path="/sign-up" element={<SignUp />}></Route>
                     <Route exact path="/detail" element={<TravelDetailPage />}></Route>
-                    <Route exact path="/result-list" element={<ResultView />}></Route>
+                    <Route exact path="/result-list/:id" element={<ResultView />}></Route>
                     <Route exact path="/preference" element={<ChoosePreference />}></Route>
                     <Route exact path="/place" element={<ChoosePlace />}></Route>
                     <Route exact path="/courseimport" element={<CourseImport />}></Route>
                     <Route exact path="/mypage/*" element={<MypageMain />}></Route>
                     <Route exact path="/coursereview/*"  element={<CourseReview />}></Route>
                     <Route exact path="/SearchPage"  element={<SearchPage />}></Route>
-                    
+
                 </Route>
             </Routes>
         </BrowserRouter>
