f9d2068d3a458c58b785472b367637f3a5d998ef
onebbu <vmfdo23@gmail.com>
2024-05-03 21:46:38 +0900

    UPDATE: Oauth 프론트 로그인 구현 완료 / 로그인+로그아웃 기능 구현 중

--- odiga_frontend/src/App.js
+++ odiga_frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/sign-up/SignUp";
 import TravelDetailPage from "./pages/traveldetailpage/TravelDetailPage";
@@ -11,26 +11,31 @@ import Main from "./pages/main/Main";
 import ResultView from "./pages/result-list/ResultView";
 import WrongPathPage from "./pages/wrongpathPage/WrongPath";
 import SearchPage from "./pages/SearchPage/SearchPage";
+import OAuthLoginInfo from "./pages/component/Oauth/OauthLoginInfo";
 
 function App() {
-    return(
+    return (
         <BrowserRouter>
             <Routes>
                 <Route>
-                    <Route exact path="/" element={<Main />}></Route>
-                    <Route exact path="/login" element={<Login />}></Route>
-                    <Route exact path="/sign-up" element={<SignUp />}></Route>
-                    <Route exact path="/detail" element={<TravelDetailPage />}></Route>
-                    <Route exact path="/result-list/:id" element={<ResultView />}></Route>
-                    <Route exact path="/preference" element={<ChoosePreference />}></Route>
-                    <Route exact path="/courseimport" element={<CourseImport />}></Route>
-                    <Route exact path="/mypage/*" element={<MypageMain />}></Route>
-                    <Route exact path="/coursereview/*"  element={<CourseReview />}></Route>
-                    <Route exact path="/SearchPage"  element={<SearchPage />}></Route>
-                    <Route exact path="/wrongpath/:nextPath"  element={<WrongPathPage />}></Route>
+                    <Route exact path="/" element={<Main/>}></Route>
+                    <Route exact path="/login" element={<Login/>}></Route>
+                    <Route exact path="/sign-up" element={<SignUp/>}></Route>
+                    <Route exact path="/detail" element={<TravelDetailPage/>}></Route>
+                    <Route exact path="/result-list/:id" element={<ResultView/>}></Route>
+                    <Route exact path="/preference" element={<ChoosePreference/>}></Route>
+                    <Route exact path="/courseimport" element={<CourseImport/>}></Route>
+                    <Route exact path="/mypage/*" element={<MypageMain/>}></Route>
+                    <Route exact path="/coursereview/*" element={<CourseReview/>}></Route>
+                    <Route exact path="/SearchPage" element={<SearchPage/>}></Route>
+                    <Route exact path="/wrongpath/:nextPath" element={<WrongPathPage/>}></Route>
                     {/* /place로 접근했을 때 /wrongpath/:nextPath로 리다이렉션합니다. */}
-                    <Route path="/place" element={<Navigate to ="/wrongpath/preference"/>}></Route>
+                    <Route path="/place" element={<Navigate to="/wrongpath/preference"/>}></Route>
                 </Route>
+
+
+                <Route exact path="/login-info" element={<OAuthLoginInfo/>}></Route>
+
             </Routes>
         </BrowserRouter>
     );
---
+++ odiga_frontend/src/pages/component/Oauth/OauthLogout.jsx
@@ -0,0 +1,33 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function handleLogout() {
+    const fetchLogout = async () => {
+        try {
+            // jwt 로그아웃의 경우
+            const token = localStorage.getItem('token');
+            localStorage.removeItem('token');
+
+            // oauth 로그아웃의 경우
+            // /auth/logout 엔드포인트로 POST 요청을 보냅니다.
+            await axios.post('/auth/logout')
+
+            // 로그아웃 후 로직
+            window.location.reload(true);
+        } catch (error) {
+            console.error('로그아웃 실패:', error);
+        }
+    };
+
+    fetchLogout();
+}
+
+function OAuthLogout() {
+    return (
+        <div>
+            <button onClick={handleLogout}>로그아웃</button>
+        </div>
+    );
+}
+
+export default OAuthLogout;
