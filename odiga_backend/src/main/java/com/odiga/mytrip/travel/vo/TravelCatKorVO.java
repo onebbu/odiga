6a5d9d360b550b116c986cc814f1c88af38225f4
sejong6033 <tpwhd6033@naver.com>
2024-05-08 11:56:05 +0900

    UPDATE : 태그목록 cat 로 기본설정 (추후 변경예정)

---
+++ odiga_backend/src/main/java/com/odiga/mytrip/travel/vo/TravelCatKorVO.java
@@ -0,0 +1,14 @@
+package com.odiga.mytrip.travel.vo;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class TravelCatKorVO {
+    private String cat1;
+    private String cat2;
+    private String cat3;
+    private String cat1kr;
+    private String cat2kr;
+    private String cat3kr;
+}
