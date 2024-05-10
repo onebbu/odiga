99cd5131fe222a63e6c7641a71f14ba8a17381d2
JAEHYEON <165257764+OGallery@users.noreply.github.com>
2024-05-10 15:20:36 +0900

    [Feature/traveldetailpage] UPDATE : 찜 삭제 기능, 리뷰 수정/삭제 기능, 평균 별점 기능  (#92)
    * ADD:여행지 상세 목록 조회 페이지 생성
    
    * UPDATE : 디테일페이지 업데이트
    
    * 구글api구현 테스트중 :
    
    google API 의존성 추가
    간단한 테스트로 api호출
    오라클DB계정 갱신
    2024.04.09
    
    * [feature/traveldetailpage] conflict resolve
    
    * feature/traveldetailpage conflict resolve
    
    * feature/traveldetailpage Delete : 구글api 테스트 삭제
    
    * ADD : /detail/{contentid}/받으면 TravelList 정보 json으로 주는 백엔드 추가
    
    * add : 여행상세페이지방문시 조회수 +1늘어나는 기능추가
    
    * UPDATE : Travel 백엔드 경로수정
    
    * UPDATE : 네이버 맵 호출
    
    * ADD : 여행지 후기 별점 작성 기능완료
    
    * ADD : 댓글 조회 기능 추가
    
    * UPDATE : 기능 버그 수정
    
    * ADD : 좋아요 기능 임시추가 , 최종결과 조회 불러오기 기능 임시추가
    
    * UPDATE : 어플리케션 프로포티 , 코스리스트 매퍼 간단수정
    
    * Delete odiga_backend/target directory
    
    * UPDATE : 태그목록 cat 로 기본설정 (추후 변경예정)
    
    * [feature/traveldetailpage] UPDATE : 기능 개선 및 추가
    
    * [feature/traveldetailpage] UPDATE : 기능 추가 및 수정
    
    * UPDATE : 여행지 상세 페이지 백단 작업 1차완료
    
    * UPDATE : 찜목록 기능 추가 , (댓글 수정 ,삭제 백엔드 작업완료) , 찜목록 삭제 기능 1차완료
    
    * UPDATE : 여행지상세페이지에서 평점 평균구하는 로직구성
    
    ---------
    
    Co-authored-by: sejong6033 <tpwhd6033@naver.com>
    Co-authored-by: onebbu <vmfdo23@gmail.com>
    Co-authored-by: Sangeun Park <101400455+onebbu@users.noreply.github.com>
    Co-authored-by: JH <outrogallery@gamil.com>
    Co-authored-by: sejong6033 <165006013+sejong6033@users.noreply.github.com>

--- odiga_frontend/src/pages/traveldetailpage/TravelDetailPage.jsx
+++ odiga_frontend/src/pages/traveldetailpage/TravelDetailPage.jsx
@@ -31,6 +31,7 @@ function TravelDetailPage() {
         axios.get(`/detail/${contentID}`)
             .then(response => {
                 setData(response.data);
+                console.log(data);
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
@@ -153,6 +154,7 @@ function TravelDetailPage() {
 //         });
 // }, []);
 
+
     return (
         <div className="inner">
             <Header/>
@@ -165,9 +167,9 @@ function TravelDetailPage() {
                 <section className="detailInfo" id="detail-info">
                     <div className="InfoAndLikeBox">
                         <h2>상세 정보</h2>
-                        <div>
+                        <div className="likeview">
                             <span id="view-count">👀 {data && (data.travelviewcount || 0)}</span>
-                            <span style={{marginLeft: '20px'}}><FontAwesomeIcon icon={faHeart} /> {likes}</span>
+                            <span style={{marginLeft: '20px'}}><FontAwesomeIcon icon={faHeart} /> {likes}</span>               
                         </div>
                     </div>
                     <div className="contourLine3"></div>
