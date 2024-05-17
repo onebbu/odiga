c2738d20363e83437adb9f3e65fe3bbfcb51b1bc
sejong6033 <165006013+sejong6033@users.noreply.github.com>
2024-05-17 15:50:31 +0900

    Update CourseImport.jsx

--- odiga_frontend/src/pages/courseimport/CourseImport.jsx
+++ odiga_frontend/src/pages/courseimport/CourseImport.jsx
@@ -9,10 +9,6 @@ import Header from "../component/navbar/Header";
 import Footer from '../component/footer/Footer';
 import './styles.css';
 import './couseImport.css';
-import Header from "../component/navbar/Header";
-import Footer from '../component/footer/Footer';
-import './styles.css';
-import './couseImport.css';
 
 const Input = styled.input`
   width: 100%;
