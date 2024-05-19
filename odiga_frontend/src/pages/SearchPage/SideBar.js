685da49c63563f3984d137cc5fb35128a713440d
Sangeun Park <101400455+onebbu@users.noreply.github.com>
2024-05-19 12:32:58 +0900

    [Feature/login] UPDATE: 검색 페이지 카테고리 선택 방식 변경(아코디언 -> 팝업) (#119)
    * ADD: 로그인 페이지 추가
    
    * [feature/login] ADD: 구글 로그인
    
    * [feature/login] 로그인 페이지 1차 UI 완료
    
    * [feature/login] UPDATE: 로그인, 회원가입 구현
    
    * CORS 설정 변경
    
    * [feature/login] UPDATE: DB 로그인 페이지 구현
    
    * [feature/login] UDATE: 로그인 페이지 유효성 검사 추가
    
    * [feature/login] 오타 수정
    
    * [feature/login] UDATE: 오타 수정
    
    * [feature/login] google login 백단 1차 완료
    
    * [feature/login] UPDATE: kakao, naver 로그인 백엔드 구현
    
    * feature/login DELETE: 구성 변경 및 필요 없는 파일 삭제
    
    * [feature/login] UPDATE: 오타 수정
    
    * UPDATE: Oauth 프론트 로그인 구현 완료 / 로그인+로그아웃 기능 구현 중
    
    * UPDATE: Oauth 로그인+로그아웃 기능 구현 완료
    
    * UPDATE: DB 로그인 유효성 검사 변경(닉네임), 최대 글자수 유효성 검사 추가(닉네임, 비밀번호)
    
    * UPDATE: MyPage로 로그인한 사용자 정보 가져오기 성공
    
    * ADD: ADD login-icons image
    
    * UPDATE: footer 경로 수정
    
    * UPDATE: 마이페이지 url 경로 수정, resultList 비밀번호 접근 로직 추가
    
    * UPDATE: 마이페이지 게시글 조회
    
    * UPDATE: 마이페이지 작성 글 UI 수정
    
    * UPDATE: 마이페이지 찜목록 조회 구현
    
    * UPDATE: JWT 토큰 만료시 로그아웃
    
    * UPDATE: 로그인 시 JWT 토큰 저장소 변경 및 시간 설정(로컬 스토리지 -> 세션 스토리지 / 토큰 1시간 후 만료)
    
    * UPDATE: 로그아웃 로직 변경(localStorage -> sessionStorage)
    
    * UPDATE: 여행지 모달창으로 조회
    
    * Delete odiga_backend/target/classes/com/odiga/mytrip/travel directory
    
    * REFACTOR: @Transactional 어노테이션 추가(DB 트랜잭션 ACID)
    
    * REFACTOR: 여행지 모달창으로 조회 에러 수정
    
    * REFACTOR: 여행지 모달창으로 조회 에러 수정
    
    * UPDATE: 검색 페이지 수정(SearchList-getSearchList 매퍼 파일 수정 필요)
    
    * UPDATE: 검색 페이지 에러 수정 완료
    
    * UPDATE: 전체적인 디자인 수정(결과 페이지, 여행지 디테일 등)
    
    * UPDATE: 검색 페이지 카테고리 선택 방식 변경(아코디언 -> 팝업)
    
    ---------
    
    Co-authored-by: sejong6033 <165006013+sejong6033@users.noreply.github.com>

--- odiga_frontend/src/pages/SearchPage/SideBar.js
+++ odiga_frontend/src/pages/SearchPage/SideBar.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
+import React, {useState, useEffect} from 'react';
 import styled from "styled-components";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import ClearIcon from '@mui/icons-material/Clear';
 
-function Sidebar({ catList, setCatCode }) {
+function Sidebar({catList, setCatCode}) {
     const [selectedCatType, setSelectedCatType] = useState(null);
     const [selectedCatCode, setSelectedCatCode] = useState(null);
 
@@ -42,76 +44,110 @@ function Sidebar({ catList, setCatCode }) {
         }
     };
 
+    // 팝오버 내용
+    const popoverContent = (
+        <Popover id="popover-cat">
+            <Popover.Body>
+                <div
+                style={{display: "flex"}}
+                >
+                    <div>
+                        {Object.keys(catTypesMap).map((cattype, index) => (
+                            <div key={index}>
+                                <CatButton
+                                    onClick={() => handleCatTypeClick(cattype)}
+                                    className={selectedCatType === cattype ? 'active' : ''}
+                                    style={{padding: '5px 10px', fontSize: '0.8rem'}}
+                                >
+                                    {cattype}
+                                </CatButton>
+                            </div>
+                        ))}
+                    </div>
+                    <div>
+                        {selectedCatType && (
+                            <div>
+                                {catTypesMap[selectedCatType].map((cat, index) => (
+                                    <div key={index}>
+                                        <CatButton
+                                            onClick={() => handleCatkrClick(cat.catcode)}
+                                            className={selectedCatCode === cat.catcode ? 'active' : ''}
+                                            style={{padding: '5px 10px', fontSize: '0.8rem'}}
+                                        >
+                                            {cat.catkr}
+                                        </CatButton>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </Popover.Body>
+        </Popover>
+    );
+
     return (
-        <div className="sidebar" style={{ float: "right" }}>
-            <TitleCat>카테고리</TitleCat>
-            {selectedCatCode && (
-                <SelectedCategoryWrapper>
-                    <SelectedCat>현재 선택: {catList.find(cat => cat.catcode === selectedCatCode)?.catkr}</SelectedCat>
-                    <SelectedCat onClick={handleCatTypeReset}>다시 선택</SelectedCat>
-                </SelectedCategoryWrapper>
-            )}
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>cattype</Accordion.Header>
-                    <Accordion.Body>
-                        <div>
-                            {Object.keys(catTypesMap).map((cattype, index) => (
-                                <div key={index}>
-                                    <CatButton
-                                        onClick={() => handleCatTypeClick(cattype)}
-                                        className={selectedCatType === cattype ? 'active' : ''}
-                                        style={{ padding: '5px 10px', fontSize: '0.8rem' }}
-                                    >
-                                        {cattype}
-                                    </CatButton>
-                                </div>
-                            ))}
-                        </div>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>catkr</Accordion.Header>
-                    <Accordion.Body>
-                        <div>
-                            {selectedCatType && (
-                                <div>
-                                    {catTypesMap[selectedCatType].map((cat, index) => (
-                                        <div key={index}>
-                                            <CatButton
-                                                onClick={() => handleCatkrClick(cat.catcode)}
-                                                className={selectedCatCode === cat.catcode ? 'active' : ''}
-                                                style={{ padding: '5px 10px', fontSize: '0.8rem' }}
-                                            >
-                                                {cat.catkr}
-                                            </CatButton>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+        <div className="sidebar">
+            <OverlayTrigger
+                trigger="click"
+                placement="bottom"
+                overlay={popoverContent}
+                rootClose
+            >
+                <CategoryButton>카테고리 선택</CategoryButton>
+            </OverlayTrigger>
+            <div>
+                {selectedCatCode && (
+                    <SelectedCategoryWrapper>
+                        <SelectedCat
+                            onClick={handleCatTypeReset}>{catList.find(cat => cat.catcode === selectedCatCode)?.catkr}
+                            <ClearIcon/></SelectedCat>
+                    </SelectedCategoryWrapper>
+                )}
+            </div>
         </div>
     );
 }
 
 export default Sidebar;
 
-const TitleCat = styled.h2`
-    text-align: center;
-`;
-
 const SelectedCategoryWrapper = styled.div`
-    text-align: center;
+  text-align: center;
 `;
 
-const SelectedCat = styled.div`
-    margin-top: 1px;
-    margin-bottom: 1px;
+const SelectedCat = styled.button`
+  background-color: #4285F4;
+  color: white;
+  border-radius: 8px;
+  padding: 5px 10px;
+  margin-right: 10px;
+  border: none;
+`;
+
+const CategoryButton = styled.button`
+  background-color: white;
+  color: #4285F4;
+  border-radius: 8px;
+  padding: 5px 10px;
+  border: black solid 1px;
+  margin-right: 10px;
 `;
 
 const CatButton = styled.button`
-    color: black;
+  color: black;
+  margin-bottom: 5px;
+  display: block;
+  width: 100%;
+  text-align: left;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  padding: 0;
+  cursor: pointer;
+  &:hover {
+    background-color: #f8f9fa;
+  }
+  &.active {
+    font-weight: bold;
+  }
 `;
