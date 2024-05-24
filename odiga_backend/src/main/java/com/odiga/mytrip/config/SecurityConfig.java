eda6dc6d80d9515dee4193d34666e17550c6badf
sejong6033 <165006013+sejong6033@users.noreply.github.com>
2024-05-24 15:14:27 +0900

    [Feature/main] UPDATE : search 쿼리문 수정및 시큐리티config 주석처리 (#137)
    * ADD: 로그인 페이지 추가
    
    * [feature/login] ADD: 구글 로그인
    
    * [feature/login] 로그인 페이지 1차 UI 완료
    
    * [feature/login] UPDATE: 로그인, 회원가입 구현
    
    * CORS 설정 변경
    
    * [feature/login] UPDATE: DB 로그인 페이지 구현
    
    * [feature/login] UDATE: 로그인 페이지 유효성 검사 추가
    
    * [feature/login] 오타 수정
    
    * [feature/login] UDATE: 오타 수정
    
    * [feature/login] google login 백단 1차 완료
    
    * [feature/login] UPDATE: kakao, naver 로그인 백엔드 구현
    
    * [feature/main] mainpage 1차 완료 by 재현님
    
    * feature/main UPDATE: 메인 업데이트
    
    * .gitignore  변경
    
    * 코드 충돌 해결
    
    * ADD : 여행코스 추천 기능 추가 (추후 여행지 추천 코스기능 추가예정.)
    
    * update : 관광지 추천기능 추가중
    
    * UPDATE : 여행지 추천기능 및 페이지 연결
    
    * UPDATE : 페이지 경로 수정
    
    * UPDATE : 더미파일 삭제 및 간단한 코드수정
    
    * UPDATE : 잡버그 수정
    
    * Delete odiga_backend/target/classes/com/odiga/mytrip/travel directory
    
    * UPDATE : 메인 태그기능
    
    * UPDATE : 헤더 마우스올릴시 손가락표시 , 리뷰별점수정 기능추가
    
    * UPDATE : search 쿼리문 수정및 시큐리티config 주석처리
    
    ---------
    
    Co-authored-by: onebbu <vmfdo23@gmail.com>
    Co-authored-by: Sangeun Park <101400455+onebbu@users.noreply.github.com>
    Co-authored-by: jaehyeon <outrogallery@gmail.com>
    Co-authored-by: JH <outrogallery@gamil.com>

--- odiga_backend/src/main/java/com/odiga/mytrip/config/SecurityConfig.java
+++ odiga_backend/src/main/java/com/odiga/mytrip/config/SecurityConfig.java
@@ -52,11 +52,11 @@ public class SecurityConfig {
                         // my-page
                         .requestMatchers("/my-page/**").hasRole("USER")
                         // generate course
-                        .requestMatchers("/MyCourseDisplay/**", "/place/savedata/**").hasRole("USER")
+                        // .requestMatchers("/MyCourseDisplay/**", "/place/savedata/**").hasRole("USER")
                         // genaerate courseReview
                         // .requestMatchers("/coursereview/commentWrite/**", "/coursereview/commentDel/**", "/coursereview/like/**", "/coursereview/likeCancel/**", "/coursereview/delete/**", "/coursereview/update/**", "/courseimport/**").hasRole("USER")
                         // Location add & delete wish
-                        .requestMatchers("/WishDelete/**", "/travelLike/**").hasRole("USER")
+                        // .requestMatchers("/WishDelete/**", "/travelLike/**").hasRole("USER")
                         .anyRequest().permitAll()
                 );
 
