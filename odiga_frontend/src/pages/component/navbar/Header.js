811c56146d81d176986daa1a44f0213f70f9a7c5
sejong6033 <165006013+sejong6033@users.noreply.github.com>
2024-05-24 16:34:30 +0900

    [Feature/main] UPDATE : 메인페이지=> 검색페이지 이동 및 마이페이지 텍스트 수정 (#138)
    * ADD: 로그인 페이지 추가
    
    * [feature/login] ADD: 구글 로그인
    
    * [feature/login] 로그인 페이지 1차 UI 완료
    
    * [feature/login] UPDATE: 로그인, 회원가입 구현
    
    * CORS 설정 변경
    
    * [feature/login] UPDATE: DB 로그인 페이지 구현
    
    * [feature/login] UDATE: 로그인 페이지 유효성 검사 추가
    
    * [feature/login] 오타 수정
    
    * [feature/login] UDATE: 오타 수정
    
    * [feature/login] google login 백단 1차 완료
    
    * [feature/login] UPDATE: kakao, naver 로그인 백엔드 구현
    
    * [feature/main] mainpage 1차 완료 by 재현님
    
    * feature/main UPDATE: 메인 업데이트
    
    * .gitignore  변경
    
    * 코드 충돌 해결
    
    * ADD : 여행코스 추천 기능 추가 (추후 여행지 추천 코스기능 추가예정.)
    
    * update : 관광지 추천기능 추가중
    
    * UPDATE : 여행지 추천기능 및 페이지 연결
    
    * UPDATE : 페이지 경로 수정
    
    * UPDATE : 더미파일 삭제 및 간단한 코드수정
    
    * UPDATE : 잡버그 수정
    
    * Delete odiga_backend/target/classes/com/odiga/mytrip/travel directory
    
    * UPDATE : 메인 태그기능
    
    * UPDATE : 헤더 마우스올릴시 손가락표시 , 리뷰별점수정 기능추가
    
    * UPDATE : search 쿼리문 수정및 시큐리티config 주석처리
    
    * UPDATE : 메인페이지=> 검색페이지 이동 및 마이페이지 텍스트 수정
    
    ---------
    
    Co-authored-by: onebbu <vmfdo23@gmail.com>
    Co-authored-by: Sangeun Park <101400455+onebbu@users.noreply.github.com>
    Co-authored-by: jaehyeon <outrogallery@gmail.com>
    Co-authored-by: JH <outrogallery@gamil.com>

--- odiga_frontend/src/pages/component/navbar/Header.js
+++ odiga_frontend/src/pages/component/navbar/Header.js
@@ -29,7 +29,7 @@ function Header() {
     }, [lastScrollY]);
 
     const handleNavigation = (path) => {
-        if (path === '/preference' && !sessionStorage.getItem('token')) {
+        if (path === '/preference' && !loginInfo) {
             const isConfirmed = window.confirm('로그인이 필요한 페이지입니다. 로그인 페이지로 이동하시겠습니까?');
             if (isConfirmed) {
                 navigate("/login", { state: { from } });
