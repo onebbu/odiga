12066322a8dd9adcb6a88a3ebfaa327f50e45cae
onebbu <vmfdo23@gmail.com>
2024-09-29 14:19:24 +0900

    UPDATE: 로그인, 회원가입 URL 경로 수정

--- odiga_frontend/src/pages/login/Logout.jsx
+++ odiga_frontend/src/pages/login/Logout.jsx
@@ -15,7 +15,7 @@ function handleLogout() {
             await axios.post('/api/auth/logout')
 
             // 로그아웃 후 로직
-            window.location.href = '/api/login';
+            window.location.href = '/login';
         } catch (error) {
             console.error('로그아웃 실패:', error);
         }
